f537d583f4604acd63d6bf305bd93b2f2863b21f
mikekavouras <kavourasm+ed-1@gmail.com>
2014-10-01 15:25:31 -0400

    move docs to docs.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-  var apiKey = "2W5eRXOm7i0ZpdlEKi3g2G6wCAOJsTBpJ45aenzuNGikak9tl2";
-  var blogName = "teeessstttt"
+var apiKey = "2W5eRXOm7i0ZpdlEKi3g2G6wCAOJsTBpJ45aenzuNGikak9tl2";
+var blogName = "teeessstttt"
 
-  var t = new TumblrMachine(blogName, apiKey, true);
+var t = new TumblrMachine(blogName, apiKey, true);
+
