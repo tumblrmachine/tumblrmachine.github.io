c2b28eab6314b44cc1a0f979c6c901ebbd58d3cb
mikekavouras <kavourasm+ed-1@gmail.com>
2014-10-15 19:09:41 -0400

    easy peasy infinite scroll

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var key = "2W5eRXOm7i0ZpdlEKi3g2G6wCAOJsTBpJ45aenzuNGikak9tl2"
 var t;
+var $window;
+var $body;
+var done = false;
+var fetching = false;
+
 function processPosts(collection, last) {
   var $photos = $('#photos');
-  var $more = $('#more');
   var posts = collection.posts('photo');
   for (var i = 0; i < posts.length; i++) {
     var post = posts[i];
@@ -10,31 +14,44 @@ function processPosts(collection, last) {
     var original = post.imageForSize('original').url;
     $photos.append('<a href="' + original + '" target="_blank"><img src="' + medium + '"></a>');
   }
-
-  $more.toggle(!last);
+  done = last;
+  fetching = false;
 }
 
 function createTumblrMachine(name) {
+  fetching = true;
   t = new TumblrMachine(name, key, false, function(collection, last) {
     processPosts(collection, last);
   });
 };
 
 function getNextPage() {
+  fetching = true;
   t.getNextPage(function(collection, last) {
     processPosts(collection, last);
   });
 }
 
+function infinite() {
+  if (done || fetching) return;
+  if ($window.scrollTop() > $body.height() - $window.height() - 300) {
+    getNextPage();
+  }
+}
+
 $(document).ready(function() {
   $('#form').bind('submit', function(e) {
     e.preventDefault();
+    done = false;
     $('#photos').empty();
     createTumblrMachine($('#blog-name').val());
   });
 
-  $('#more').bind('click', function() {
-    getNextPage();
+  $window = $(window);
+  $body = $('body');
+  $window.bind('scroll', function() {
+    infinite();
   });
+
 });
 
